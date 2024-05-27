a636169e272f2c489308233560dd0854bd22f6a4
jeanphilippecyr <2095835@csfoy.ca>
2024-05-27 14:33:06 -0400

    Creation de la page 404

---
+++ tp2-webapp/src/pages/Page404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Page404() {
+    return (
+        <div>
+            <h1>404 Non trouvé</h1>
+            <p>La page que vous recherchez n'existe pas.</p>
+        </div>
+    );
+}
+export default Page404;
