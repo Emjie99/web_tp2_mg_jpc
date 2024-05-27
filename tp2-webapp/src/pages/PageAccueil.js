60cab17521f4b99f64691d4f8f4c33ffc45a94b9
Maxime Gaulin <maximegaulin@gmail.com>
2024-05-27 14:32:10 -0400

    removed manifest + logo192 references in index.html

---
+++ tp2-webapp/src/pages/PageAccueil.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const PageAccueil = () => {
+    return (
+        <div>
+            <h1>
+                Bienvenue!
+            </h1>
+            <h2>
+                TP2 Jean-Philippe Cyr et Maxime Gaulin
+            </h2>
+            <p>
+                Bienvenue à notre page d'accueil. Pour continuer, veuillez vous authentifier.
+            </p>
+        </div>
+    )
+}
+
+export default PageAccueil;
